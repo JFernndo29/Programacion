db24aa13047e27c3338473287d15e295dbfa723b
MariaChalenm <99472024+MariaChalenm@users.noreply.github.com>
2022-02-11 02:57:53 -0600

    Update Libreria.java
    Es esa clase creaste las variables String, double, int que son dos se guardaran valores cómo: precio, título del libro, cantidad en está clase buscamos los diferentes libros por títulolibro, preciolibro, cantidadlibro

--- Libreria.java
+++ Libreria.java
@@ -2,3 +2,74 @@
 public class Libreria {
 
 }
+
+public class Libreria implements Comparable<Libreria> {
+	private String isbn;
+	private String titulolibro;
+	private double preciolibro;
+	private int cantidadlibro;
+
+	public Libreria() {
+		
+	}
+	
+	public Libreria(String isbn, String titulolibro, double preciolibro, int cantidadlibro) {
+		this.isbn = isbn;
+		this.titulolibro = titulolibro;
+		this.preciolibro = preciolibro;
+		this.cantidadlibro = cantidadlibro;
+	}
+	
+	// getters y setters
+	public String getIsbn() {
+		return isbn;
+	}
+	
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	
+	public String getTitulo() {
+		return titulolibro;
+	}
+	
+	public void setTitulo(String titulolibro) {
+		this.titulolibro = titulolibro;
+	}
+	
+	public double getPrecio() {
+		return preciolibro;
+	}
+	
+	public void setPrecio(double preciolibro) {
+		this.preciolibro = preciolibro;
+	}
+	
+	public int getCantidadLibro() {
+		return cantidadlibro;
+	}
+	
+	public void setCantidad(int cantidadlibro) {
+		this.cantidadlibro = cantidadlibro;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Libreria l = (Libreria)obj;
+		return this.isbn.equals(l.isbn);
+	}
+
+	@Override
+	public String toString() {
+		return this.isbn + " - " + this.titulolibro + " - " +"$"+ this.preciolibro + " - " + this.cantidadlibro; 
+	}
+	
+	@Override
+	public int compareTo(Libreria l) {
+		if(l.getPrecio() < preciolibro) {
+			return -1;
+		}else {
+			return 1;
+		}
+	}
+}

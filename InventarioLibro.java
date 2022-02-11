7ee274a5440e4afd9b49d2bf44e49bee3e0125da
Stephany Almache <90081400+Imstephany@users.noreply.github.com>
2022-02-11 02:26:36 -0600

    Update InventarioLibro.java

--- InventarioLibro.java
+++ InventarioLibro.java
@@ -1,4 +1,36 @@
-
+import java.util.ArrayList;
 public class InventarioLibro {
 
+	private ArrayList<Libreria> libros = new ArrayList<Libreria>();
+	
+	public void almacenar(Libreria libro) {
+		libros.add(libro);
+	}
+	
+	public Libreria getLibrobyIsbn(String isbn) {
+		for(Libreria l: libros) {
+			if(l.getIsbn().equals(isbn)) {
+				return l;
+			}
+		}
+		return null;
+	}
+	
+	public void verLibreria() {
+		for(Libreria lb: libros) {
+			System.out.println(lb);
+		}
+	}
+
+	public ArrayList<Libreria> getLibros() {
+		return libros;
+	}
+	
+	public void ventaLibro(Libreria libro, int cantidadlibro) {
+		if(libro.getCantidadLibro()<cantidadlibro) {
+			System.out.println("No hay stock");
+		}else if(libro.getCantidadLibro()>=cantidadlibro) {
+			libro.setCantidad(libro.getCantidadLibro()-cantidadlibro);
+		}
+	}
 }
